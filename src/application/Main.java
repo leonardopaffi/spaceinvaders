ab16122d0ef9a59a6f4d0a920419eb27b339ff26
Shindanaide <leonardopaffi@hotmail.it>
2015-03-06 22:36:42 +0100

    Movement left-right
    Now the ship moves left and right

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,20 +5,34 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
-	
+	ImageView ship = new ImageView();
 	@Override
 	public void start(Stage primaryStage) {
-		Image mainChLoad = new Image("application/res/ship.png");
-		ImageView ship = new ImageView(mainChLoad);
-		Pane pane = new Pane();
-		pane.getChildren().add(ship);
-		Scene scene = new Scene(pane);
+		Image mainChLoad = new Image("/application/res/ship.png", true); //loads ship
+		ship.setImage(mainChLoad);  //show ship
+		Pane pane = new Pane(); 
+		pane.getChildren().add(ship); //adds the ship to Pane
+		Scene scene = new Scene(pane, 600, 600);
+		scene.setOnKeyPressed(e-> keys(e)); //keyboard event
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	public void keys(KeyEvent ke){
+		if(ke.getCode() == KeyCode.D){
+			double x = ship.getX()+10;
+			ship.setX(x);
+		}
+		if(ke.getCode() == KeyCode.A){
+			double x = ship.getX()-10;
+			ship.setX(x);
+		}
 	}
 	
 	public static void main(String[] args) {
