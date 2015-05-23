60cdadf7c785abeea48aaccc0c6e2890941234fa
Leonardo Paffi <leonardopaffi@hotmail.it>
2015-05-23 21:45:14 +0200

    Background
    -New Background
    -Score system improved
    
    TODO:
    -Score system update
    -General fixes

--- src/it/gov/casimiri/spaceinvaders/Bullet.java
+++ src/it/gov/casimiri/spaceinvaders/Bullet.java
@@ -9,64 +9,70 @@ import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class Bullet {
-	boolean bulletIsAlive = true;
-	Rectangle r = null;
-	Pane p;
-	int incr;
-	
-	public Bullet (double x, double y, double pos, ImageView v[], Pane p){
-		this.p = p;
-		Timeline tlB;
-		r = new Rectangle (x, y);
-		p.getChildren().add(r);
-        r.setX(pos+35);
-        r.setY(640);
-        Duration dB = new Duration(5);
-        KeyFrame fB = new KeyFrame(dB, e -> {
-        	if(r != null){
-            	r.setY(r.getY()-5);
-            	collisioncheck(v);
-        	}
-        });
-        tlB = new Timeline(fB);
-        tlB.setCycleCount(Animation.INDEFINITE);
-        tlB.play();
-	}
-	
-	private boolean collisioncheck (ImageView enemies[]){
-    	for (int i = 0; i < enemies.length; i++) {
-    		if (r != null && enemies[i] != null){
-    			if ((r != null && r.getX() < enemies[i].getX() + enemies[i].getFitWidth()
-    			&& r.getX() + r.getWidth() > enemies[i].getX()
-    		    && r.getY() < enemies[i].getY() + enemies[i].getFitHeight()
-    		    && r.getHeight() + r.getY() > enemies[i].getY()))
-    			{
-    				System.out.println("DENIED");
-        			enemies[i].setVisible(false);
-        			enemies[i] = null;
-        			removeBullet();
-        			r= null;
-        			bulletIsAlive = false;
-        			incr+=50;
-    			}
-    		}
-    	}
-    	if(r!=null){
-    		if(r.getY()<=0){
-    			removeBullet();
-    		}
-    	}
-    	return bulletIsAlive;
+
+    boolean bulletIsAlive = true;
+    Rectangle r = null;
+    Pane p;
+    int incr;
+    static Rectangle rect = Main.pointer;
+
+    public Bullet(double x, double y, double pos, ImageView v[], Pane p) {
+        if (r == null) {
+            this.p = p;
+            Timeline tlB;
+            r = new Rectangle(x, y);
+            p.getChildren().add(r);
+            r.setX(pos + 35);
+            r.setY(640);
+            Duration dB = new Duration(5);
+            KeyFrame fB = new KeyFrame(dB, e -> {
+                if (r != null) {
+                    r.setY(r.getY() - 5);
+                    collisioncheck(v);
+                }
+            });
+            tlB = new Timeline(fB);
+            tlB.setCycleCount(Animation.INDEFINITE);
+            tlB.play();
+        }
     }
-	
-	public int getScore (){
-		int x = incr;
-		incr = 0;
-		return x;
-	}
-	
-	public void removeBullet(){
-		p.getChildren().remove(r);
-		r= null;
-	}
+
+    private boolean collisioncheck(ImageView enemies[]) {
+        for (int i = 0; i < enemies.length; i++) {
+            if (r != null && enemies[i] != null) {
+                if ((r != null && r.getX() < enemies[i].getX() + enemies[i].getFitWidth()
+                        && r.getX() + r.getWidth() > enemies[i].getX()
+                        && r.getY() < enemies[i].getY() + enemies[i].getFitHeight()
+                        && r.getHeight() + r.getY() > enemies[i].getY())) {
+                    System.out.println("DENIED");
+                    enemies[i].setVisible(false);
+                    enemies[i] = null;
+                    removeBullet();
+                    bulletIsAlive = false;
+                    incr += 50;
+                }
+            }
+        }
+        if (r != null) {
+                if (r.getY()<0-r.getHeight()-1) {
+                    removeBullet();
+            }
+        }
+        return bulletIsAlive;
+    }
+
+    public int getScore() {
+        int x = incr;
+        incr = 0;
+        return x;
+    }
+    
+    public void getRectangle(Rectangle rect){
+        this.rect = rect;
+    }
+    
+    public void removeBullet() {
+        p.getChildren().remove(r);
+        r = null;
+    }
 }
--- src/it/gov/casimiri/spaceinvaders/Main.java
+++ src/it/gov/casimiri/spaceinvaders/Main.java
@@ -1,7 +1,9 @@
 package it.gov.casimiri.spaceinvaders;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
-
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -17,162 +19,211 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
- * @author 4°B 2014/2015 R.Casimiri, Gualdo Tadino (PG)
+ * @author 4B 2014/2015 R.Casimiri, Gualdo Tadino (PG)
  */
-
 public class Main extends Application {
-	Bullet bulletc;
-	MediaPlayer mp;
-	Pane pane = new Pane();
-	Image shipV = new Image("it/gov/casimiri/spaceinvaders/resources/ship.png");
-	ImageView ship = new ImageView(shipV);
-	Image enemiesV = new Image(
-			"it/gov/casimiri/spaceinvaders/resources/invasore1.gif");
-	Image enemiesVI = new Image(
-			"it/gov/casimiri/spaceinvaders/resources/invasore2.gif");
-	ImageView[] enemies = new ImageView[12];
-	Rectangle pointer = new Rectangle();
-	String statusMP;
 
-	public static final int SCREEN_WIDTH = 800;
-	public static final int SCREEN_HEIGHT = 800;
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 800;
 
-	public static final int ENEMY_EDGE = 40;
-	public static final int ENEMY_ROW = 3;
-	public static final int ENEMY_COLUMN = 4;
+    public static final int ENEMY_EDGE = 40;
+    public static final int ENEMY_ROW = 7;
+    public static final int ENEMY_COLUMN = 7;
 
-	public static final int SPEED = 3;
+    public static final int SPEED = 3;
 
-	public int MOV = 0;
+    public int record = 0;
 
-	boolean rightEnemy = true;
-	boolean bulletIsAlive = false;
-	
-	int score = 0;
-	Text punt = new Text("Score: "+score);
+    Bullet bulletc;
+    MediaPlayer mp;
+    Pane pane = new Pane();
+    Image backV = new Image("it/gov/casimiri/spaceinvaders/resources/back.jpg");
+    ImageView back = new ImageView(backV);
+    Image shipV = new Image("it/gov/casimiri/spaceinvaders/resources/ship.png");
+    ImageView ship = new ImageView(shipV);
+    Image enemiesV = new Image(
+            "it/gov/casimiri/spaceinvaders/resources/invasore1.gif");
+    Image enemiesVI = new Image(
+            "it/gov/casimiri/spaceinvaders/resources/invasore2.gif");
 
-	@Override
-	public void start(Stage primaryStage) {
-		URL resource = getClass().getResource("resources/attacco.mp3");
-		Media media = new Media(resource.toString());
-		mp = new MediaPlayer(media);
+    static Rectangle pointer = new Rectangle();
+    String statusMP;
+    ImageView[] enemies = new ImageView[ENEMY_COLUMN * ENEMY_ROW];
 
-		ship.setPreserveRatio(true);
-		ship.setFitWidth(80);
-		ship.setX(100);
-		ship.setY(680);
+    public int MOV = 0;
 
-		for (int j = 0; j < ENEMY_ROW; j++) {
-			for (int i = 0; i < ENEMY_COLUMN; i++) {
-				// (i*50, j*50, ENEMY_EDGE, ENEMY_EDGE)
-				enemies[j * ENEMY_COLUMN + i] = new ImageView(enemiesV);
-				enemies[j * ENEMY_COLUMN + i].setPreserveRatio(true);
-				enemies[j * ENEMY_COLUMN + i].setX(i * 50);
-				enemies[j * ENEMY_COLUMN + i].setY(j * 50);
-				enemies[j * ENEMY_COLUMN + i].setFitWidth(ENEMY_EDGE);
-				pane.getChildren().add(enemies[j * 4 + i]);
-				if (i == ENEMY_COLUMN - 1 && j == 0) {
-					pointer.setWidth(ENEMY_EDGE);
-					pointer.setHeight(ENEMY_EDGE);
-					pointer.setFill(Color.TRANSPARENT);
-					pointer.setX(enemies[i].getX() + ENEMY_EDGE);
-				}
-			}
-		}
+    boolean rightEnemy = true;
+    boolean bulletIsAlive = false;
+    boolean newLevel = true;
 
-		pane.getChildren().add(ship);
-		pane.getChildren().add(punt);
-		
-		punt.setX(100);
-		punt.setY(100);
+    int score = 0;
+    int updateTime = 28;
+    Text punt = new Text("Score: " + score);
 
-		Duration dI = new Duration(25);
-		KeyFrame f = new KeyFrame(dI, e -> movementCore());
-		Timeline tl = new Timeline(f);
-		tl.setCycleCount(Animation.INDEFINITE);
-		tl.play();
-		Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT);
-		scene.setOnKeyPressed(e -> keyboardManage(e));
+    String sRecord = "";
 
-		mp.setCycleCount(MediaPlayer.INDEFINITE);
-		mp.play();
+    public void again() {
+        for (int j = 0; j < ENEMY_ROW; j++) {
+            for (int i = 0; i < ENEMY_COLUMN; i++) {
+                enemies[j * ENEMY_COLUMN + i] = new ImageView(enemiesV);
+                enemies[j * ENEMY_COLUMN + i].setPreserveRatio(true);
+                enemies[j * ENEMY_COLUMN + i].setX(i * 50);
+                enemies[j * ENEMY_COLUMN + i].setY(j * 50);
+                enemies[j * ENEMY_COLUMN + i].setFitWidth(ENEMY_EDGE);
+                pane.getChildren().add(enemies[j * ENEMY_COLUMN + i]);
+                if (i == ENEMY_COLUMN - 1 && j == 0) {
+                    pointer.setWidth(ENEMY_EDGE);
+                    pointer.setHeight(ENEMY_EDGE);
+                    pointer.setFill(Color.TRANSPARENT);
+                    pointer.setX(enemies[i].getX() + ENEMY_EDGE);
+                }
+            }
+        }
+        updateTime-=3;
+    }
 
-		primaryStage.setTitle("SpaceInvaders");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+    @Override
+    public void start(Stage primaryStage) {
 
-	public void keyboardManage(KeyEvent ke) {
-		if (ke.getCode() == KeyCode.D) {
-			double x = ship.getX();
-			x += 10;
-			ship.setX(x);
-		} else if (ke.getCode() == KeyCode.A) {
-			double x = ship.getX();
-			x -= 10;
-			ship.setX(x);
-		} else if (ke.getCode() == KeyCode.SPACE) { // shoot
-			bulletc = new Bullet(10, 50, ship.getX(), enemies, pane);
-		}
-		if (bulletc != null){
-		score+=bulletc.getScore();
-		punt.setText("Score: "+score);
-		}
-	}
+        URL resource = getClass().getResource("resources/attacco.mp3");
+        Media media = new Media(resource.toString());
+        mp = new MediaPlayer(media);
 
-	public void movementCore() {
-		if (rightEnemy) {
-			if (pointer.getX() + ENEMY_EDGE >= SCREEN_WIDTH) {
-				rightEnemy = false;
-				for (int i = 0; i < enemies.length; i++) {
-					if (enemies[i] != null) {
-						enemies[i].setY(enemies[i].getY() + 50);
-					}
-				}
-			}
-			for (int i = 0; i < enemies.length; i++) {
-				if (enemies[i] != null) {
-					enemies[i].setX(enemies[i].getX() + SPEED);
-				}
-			}
-			pointer.setX(pointer.getX() + SPEED);
-		} else {
-			if (pointer.getX()
-					- ((ENEMY_EDGE * ENEMY_COLUMN ) / 2 * (ENEMY_COLUMN - 1)) <= 0) {
-				rightEnemy = true;
-				for (int i = 0; i < enemies.length; i++) {
-					if (enemies[i] != null) {
-						enemies[i].setY(enemies[i].getY() + 50);
-					}
-				}
-			}
-			for (int i = 0; i < enemies.length; i++) {
-				if (enemies[i] != null) {
-					enemies[i].setX(enemies[i].getX() - SPEED);
-				}
-			}
-			pointer.setX(pointer.getX() - SPEED);
-		}
-		MOV++;
-		if (MOV == 20) {
-			for (int j = 0; j < enemies.length; j++) {
-				if(enemies[j]!= null)
-					enemies[j].setImage(enemiesVI);
-			}
-		} else if (MOV == 40){
-			for (int j = 0; j < enemies.length; j++) {
-				if(enemies[j] != null)
-				enemies[j].setImage(enemiesV);
-			}
-		MOV = 0;
-		}
-	}
+        ship.setPreserveRatio(true);
+        ship.setFitWidth(80);
+        ship.setX(100);
+        ship.setY(680);
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        pane.getChildren().add(back);
+        pane.getChildren().add(ship);
+        pane.getChildren().add(punt);
+
+        punt.setFont(Font.font("Verdana", 20));
+        punt.setFill(Color.YELLOW);
+
+        again();
+
+        back.setX(0);
+        back.setY(0);
+
+        punt.setX(10);
+        punt.setY(20);
+
+        Duration dI = new Duration(updateTime);
+        KeyFrame f = new KeyFrame(dI, e -> movementCore());
+        Timeline tl = new Timeline(f);
+        tl.setCycleCount(Animation.INDEFINITE);
+        tl.play();
+        Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT);
+        scene.setOnKeyPressed(e -> keyboardManage(e));
+
+        mp.setCycleCount(MediaPlayer.INDEFINITE);
+        mp.play();
+
+        primaryStage.setTitle("SpaceInvaders");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        String k = System.getProperty("user.home");
+        String n = k + File.separator + "si.txt";
+        System.out.println(n);
+
+        try {
+            FileReader fr = new FileReader(n);
+            BufferedReader br = new BufferedReader(fr);
+            sRecord = br.readLine();
+            record = Integer.parseInt(sRecord);
+            System.out.println("" + record);
+            br.close();
+            fr.close();
+        } catch (Exception e) {
+            record = 0;
+        }
+    }
+
+    public void keyboardManage(KeyEvent ke) {
+        if (ke.getCode() == KeyCode.D) {
+            double x = ship.getX();
+            x += 10;
+            ship.setX(x);
+        } else if (ke.getCode() == KeyCode.A) {
+            double x = ship.getX();
+            x -= 10;
+            ship.setX(x);
+        } else if (ke.getCode() == KeyCode.SPACE) { // shoot
+            bulletc = new Bullet(10, 50, ship.getX(), enemies, pane);
+        }
+    }
+
+    public void movementCore() {
+        if (rightEnemy) { //check if the enemy is going toward right
+            if (pointer.getX() + ENEMY_EDGE >= SCREEN_WIDTH) { //check collision on right edge
+                rightEnemy = false;
+                for (int i = 0; i < enemies.length; i++) {
+                    if (enemies[i] != null) {
+                        enemies[i].setY(enemies[i].getY() + 50);
+                    }
+                }
+            }
+            for (int i = 0; i < enemies.length; i++) {
+                if (enemies[i] != null) {
+                    enemies[i].setX(enemies[i].getX() + SPEED); //move the enemy 
+                }
+            }
+            pointer.setX(pointer.getX() + SPEED); //move the pointer
+        } else {
+            if (pointer.getX() - ((ENEMY_EDGE * (ENEMY_COLUMN + 2))) <= 0) {
+                rightEnemy = true;
+                for (int i = 0; i < enemies.length; i++) {
+                    if (enemies[i] != null) {
+                        enemies[i].setY(enemies[i].getY() + 50);
+                    }
+                }
+            }
+            for (int i = 0; i < enemies.length; i++) {
+                if (enemies[i] != null) {
+                    enemies[i].setX(enemies[i].getX() - SPEED);
+                }
+            }
+            pointer.setX(pointer.getX() - SPEED);
+        }
+        MOV++; //animation block 
+        if (MOV == 20) { //first frame
+            for (int j = 0; j < enemies.length; j++) {
+                if (enemies[j] != null) {
+                    enemies[j].setImage(enemiesVI);
+                }
+            }
+        } else if (MOV == 40) { //second frame
+            for (int j = 0; j < enemies.length; j++) {
+                if (enemies[j] != null) {
+                    enemies[j].setImage(enemiesV);
+                }
+            }
+            MOV = 0;
+        }
+        if (bulletc != null) {
+            score += bulletc.getScore();
+            punt.setText("Score: " + score);
+        }
+        if (score % 50 * 49 == 0 && score >= 50 * 49) {
+            if (newLevel) {
+                again();
+                newLevel = false;
+            }
+        }
+        if (score % 50 * 49 > 0 && score > 50 * 49) {
+            newLevel = true;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
